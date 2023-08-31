2be3fc5ddcb7176aff5bb0f724a39a494dda6f90
Jonah88877 <88877@glr.nl>
2023-08-31 12:44:31 +0200

    let naar const veranderd en code wat bij andere werkte van stackoverflow toegevoegd

--- script/script.js
+++ script/script.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let form = document.getElementById("cardForm");
-    let cardContainer = document.getElementById("cardContainer");
+    const cardForm = document.getElementById("cardForm");
+    const cardOutput = document.getElementById("cardOutput");
 
-    form.addEventListener("submit", function (event) {
+    cardForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        let Titel = document.getElementById("cardTitel").value;
-        let Beschrijving = document.getElementById("cardBesch").value;
-        let Link = document.getElementById("buttonLink").value;
-        let cardHTML = '<div class="card w-50 mb-3">' +
-            '<img src="../imgs/18410.jpg" class="card-img-top" alt="placeholder">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">' + Titel + '</h5>' +
-            '<p class="card-text">' + Beschrijving + '</p>' +
-            '<a href="' + Link + '" class="btn btn-primary" target="_blank">Naar het project</a>' +
-            '</div></div>';
+        const Titel = document.getElementById("cardTitel").value;
+        const Beschrijving = document.getElementById("cardBesch").value;
+        const Link = document.getElementById("buttonLink").value;
 
-        cardContainer.insertAdjacentHTML("beforeend", cardHTML);
+        const cardHTML = `
+        <div class="col-md-6">
+            <div class="card mb-3">
+                <img src="../imgs/18410.jpg" class="card-img-top" alt="placeholder">
+                <div class="card-body">
+                    <h5 class="card-title">${Titel}</h5>
+                    <p class="card-text">${Beschrijving}</p>
+                    <a href="${Link}" class="btn btn-primary">Naar het project</a>
+                </div>
+            </div>
+        </div>`;
 
-        form.reset();
+        cardOutput.insertAdjacentHTML("beforeend", cardHTML);
+
+        cardForm.reset();
     });
 });
