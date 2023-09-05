b447d4296f179ebcc6d4836815fb08a08b314c0e
088877 <088877@glr.nbl>
2023-09-05 15:23:24 +0200

    navbar geupdate zodat die hetzelfde is als de andere

--- Contact/contactverwerken_view.php
+++ Contact/contactverwerken_view.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <meta charset="UTF-8">
-    <title>Admin Panel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="../style/style.css" rel="stylesheet">
+    <title>Contact</title>
     <?php require_once('contact_verwerken.php'); ?>
 </head>
 <body>
 <nav class="py-3 bg-white">
     <div class="container d-flex flex-wrap">
         <ul class="nav me-auto">
-            <li class="nav-item"><a href="../Homepagina/index.html" class="nav-link link-dark px-2 fs-5" aria-current="page">Jonah</a></li>
+            <img src="../imgs/088877.jpeg" alt="foto" class="foto">
+            <li class="nav-item">
+                <a href="../Homepagina/index.html" class="nav-link link-dark px-2 fs-5" aria-current="page">
+                    <span class="typewriter">Jonah</span>
+                </a>
+            </li>
         </ul>
         <ul class="nav">
             <li class="nav-item"><a href="../Homepagina/index.html" class="nav-link link-dark px-2 fs-5" aria-current="page">Home</a></li>
             <li class="nav-item"><a href="../Introductie/intro.html" class="nav-link link-dark px-2 fs-5" aria-current="page">Wie ben ik</a></li>
             <li class="nav-item"><a href="../Projecten/projecten.html" class="nav-link link-dark px-2 fs-5" aria-current="page">Projecten</a></li>
-            <li class="nav-item"><a href="../Contact/contact.html" class="nav-link link-dark px-2 fs-5 active" aria-current="page">Contact</a></li>
+            <li class="nav-item"><a href="contact.html" class="nav-link link-dark px-2 fs-5" aria-current="page">Contact</a></li>
         </ul>
     </div>
 </nav>
